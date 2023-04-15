b73aed4381fa5b557e86d454899132415a3e92de
Darin Cassler <darin@cassler.net>
2023-04-15 14:48:26 -0500

    chore: tidy up Chat component

--- src/Chat.tsx
+++ src/Chat.tsx
@@ -13,6 +13,39 @@ export function Avatar({role}:{role: ChatCompletionRequestMessage['role']}) {
   }
 }
 
+export const ChatBubble = ({message}: {message:ChatCompletionRequestMessage}) => {
+  return (
+    <Transition.Child
+      enter="transition-all ease-linear duration-300"
+      enterFrom="opacity-0 scale-95 translate-y-4"
+      enterTo="opacity-100 scale-100 translate-y-0"
+      leave="transition-opacity duration-150"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className='grid grid-cols-[1fr,5fr] p-2 self-start'>
+        <h4 className='w-16 font-semibold tracking-tight'>
+          <Avatar role={message.role} />
+        </h4>
+        <div className='text-sm pr-4 space-y-3'>
+          {message.content.split(/\n/).map((i,idx) => <p key={`para-${idx}`}>{i}</p>)}
+        </div>
+      </div>
+    </Transition.Child>
+  )
+}
+
+export const ThinkingNode = () => (
+  <div className='self-center bg-slate-300 animate-pulse  text-white m-1 rounded h-auto items-center flex justify-center'>
+    <p className='text-sm uppercase p-4 font-light tracking-wide'>thinking...</p>
+  </div>
+)
+export const SplashMessage = () => (
+  <div className='self-center bg-slate-100 h-full items-center flex justify-center'>
+    <p className='text-2xl p-4 tracking-tight animate-pulse'>Hi there.<br />Type a message to get started</p>
+  </div>
+)
+
 export const Chat = ({className}: {className: string}) => {
   const [use4, setUse4] = useState<boolean>(false)
   const chat = useChat(use4);
@@ -20,64 +53,44 @@ export const Chat = ({className}: {className: string}) => {
 
   const { fetchChatCompletions,chatLogRef, isLoading, error, nonSystemMessages }  = chat;
 
+  function sendMessage(e:React.SyntheticEvent) {
+    e.preventDefault();
+    setValue('');
+    fetchChatCompletions(value);
+    return;
+  }
   return (
     <div className={['relative', className].join(' ')}>
-
-      <Transition as='div' show className='h-[calc(100%-60px)] bg-slate-50 overflow-auto divide-y' ref={chatLogRef}>
-
-        <>
+      <Transition
+        as='div'
+        show
+        className='h-[calc(100%-60px)] bg-slate-50 overflow-auto divide-y'
+        ref={chatLogRef}
+      >
         {error && error.message}
-        {nonSystemMessages.map((m, idx) => (
-          <Transition.Child
-            enter="transition-all ease-linear duration-300"
-            enterFrom="opacity-0 scale-95 translate-y-4"
-            enterTo="opacity-100 scale-100 translate-y-0"
-            leave="transition-opacity duration-150"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-            key={`${m.role}-${idx}`}
-          >
-            <div className='grid grid-cols-[1fr,5fr] p-2 self-start' key={`${m.role}-${idx}`}>
-              <h4 className='w-16 font-semibold tracking-tight'>
-                <Avatar role={m.role} />
-              </h4>
-              <div className='text-sm pr-4 space-y-3'>
-                {m.content.split(/\n/).map((i,idx) => <p key={`para-${idx}`}>{i}</p>)}
-              </div>
-            </div>
-          </Transition.Child>
-        ))}
-        {isLoading && (
-          <div className='self-center bg-slate-300 animate-pulse  text-white m-1 rounded h-auto items-center flex justify-center'>
-            <p className='text-sm uppercase p-4 font-light tracking-wide'>thinking...</p>
-          </div>
-        )}
-        {!isLoading && nonSystemMessages.length === 0 && (
-          <div className='self-center bg-slate-100 h-full items-center flex justify-center'>
-            <p className='text-2xl p-4 tracking-tight animate-pulse'>Hi there.<br />Type a message to get started</p>
-          </div>
-        )}
-        </>
+        {nonSystemMessages.map((m, idx) => <ChatBubble message={m} />)}
+        {isLoading && <ThinkingNode />}
+        {!isLoading && nonSystemMessages.length === 0 && <SplashMessage />}
       </Transition>
 
       <div className='p-2 bg-slate-100 border-t bottom-0 absolute w-full'>
-      <form
-        className='group flex w-full rounded gap-2'
-        onSubmit={(e) => {
-          e.preventDefault();
-          setValue('')
-          fetchChatCompletions(value)
-        }}
-      >
-        <input
-          className='px-4 py-2 focus:outline-none rounded-l shadow-inner border flex-grow group-focus-within:ring-2'
-          type="text"
-          value={value}
-          onChange={(e) => setValue(e.target.value)} />
-
-        <button className='p-2 px-3 rounded bg-white border text-indigo-600 font-medium tracking-tight group-focus-within:ring-2' type='submit'>Send</button>
-      </form>
-        <Switch checked={use4} onChange={setUse4} as='div' className='cursor-pointer flex flex-shrink-0 items-center gap-2 text-xs pt-2 ml-auto mr-0 text-slate-500 justify-end'>
+        <form className='group flex w-full rounded gap-2' onSubmit={sendMessage}>
+          <input
+            className='px-4 py-2 focus:outline-none rounded-l flex-grow border'
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)} />
+          <button
+            className='p-2 px-3 rounded bg-white border text-indigo-600 font-medium'
+            type='submit'>
+            Send
+          </button>
+        </form>
+        <Switch
+          checked={use4}
+          onChange={setUse4}
+          as='div'
+          className='cursor-pointer flex text-xs py-1 items-center gap-2 text-slate-500 justify-end'>
           <div className='flex-shrink-0'>Use GPT-4</div>
           <input readOnly type='checkbox' checked={use4} />
         </Switch>
