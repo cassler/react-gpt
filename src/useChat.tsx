7a6b20ac722eb8775d6af96a2941c60b2bf67cc5
Darin Cassler <darin@cassler.net>
2023-04-15 11:20:52 -0500

    feat: hook for facilitating a chat with OpenAI

---
+++ src/useChat.tsx
@@ -0,0 +1,62 @@
+import { ChatCompletionRequestMessage, Configuration, CreateChatCompletionRequest, OpenAIApi } from "openai";
+import { useEffect, useRef, useState } from "react";
+import { createSystemPrompt } from "./createSystemPrompt";
+
+const configuration = new Configuration({
+    organization: import.meta.env.VITE_OPENAI_ORG,
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export const useChat = (useGpt4: boolean = false) => {
+  const [chatCompletions, setChatCompletions] = useState<unknown>([])
+  const [messages, setMessages] = useState(() => createSystemPrompt())
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState<Error | null>(null)
+  const chatLogRef = useRef(null);
+
+  const fetchChatCompletions = async (message: string) => {
+    let thisMessage:ChatCompletionRequestMessage = { "role": "user", "content": message }
+    setIsLoading(true)
+    setMessages([...messages, thisMessage])
+    try {
+      const { data } = await openai.createChatCompletion({
+        model: useGpt4 ? 'gpt-4' : 'gpt-3.5-turbo',
+        messages: [...messages, thisMessage]
+      })
+      setChatCompletions(data)
+      if (data.choices[0].message) {
+        setMessages([...messages, thisMessage, data.choices[0].message])
+      }
+    }
+    catch (error) {
+      setError(error as Error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (chatLogRef.current) {
+      // @ts-ignore
+      chatLogRef.current.scrollTop = chatLogRef.current.scrollHeight
+    }
+  }, [chatLogRef, messages])
+
+  // useEffect(() => {
+  //   async () => await fetchChatCompletions('Hi there')
+  // }, [])
+
+  const nonSystemMessages = messages.filter(i => i.role !== 'system')
+
+  return {
+    chatCompletions,
+    isLoading,
+    error,
+    fetchChatCompletions,
+    messages,
+    nonSystemMessages,
+    chatLogRef
+  }
+}
